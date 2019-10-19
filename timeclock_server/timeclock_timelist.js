9b91638dc51b8fa8f62e963cb9913f5963e081a8
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-10-19 09:43:44 +0200

    Storage returning a copy of entries, list showing events in reverse order

--- timeclock_server/timeclock_timelist.js
+++ timeclock_server/timeclock_timelist.js
@@ -19,7 +19,6 @@ exports.TimeList = (storage) => {
 }
 
 function localTime(timestamp) {
-	console.log(timestamp)
 	let date = new Date(timestamp * 1000)
 	const Y = date.getFullYear()
 	const M = date.getMonth() + 1
