48dbe1cab1f61ad719a714e2c1732b0cb7d35a2d
sbohmann <mutabor@yeoman.at>
2019-10-24 14:08:27 +0200

    Report Generation

--- timeclock_server/report/days.js
+++ timeclock_server/report/days.js
@@ -13,6 +13,7 @@ function Days() {
         },
         createReport: () => {
             console.log(eventsForDay)
+            console.log(eventsForDay.keys())
             let sortedDates = Array.from(eventsForDay.keys())
             console.log(sortedDates)
             sortedDates.sort()
