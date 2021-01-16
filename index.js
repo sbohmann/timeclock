67541918b9629cdd72f822471dc9a8e6cee946d1
bohmanns <Wgw-rQf-Xqr-o8S>
2021-01-16 17:32:53 +0100

    Upload

--- index.js
+++ index.js
@@ -157,10 +157,9 @@ function handleUpload(request, response) {
         fs.mkdirSync(uploads_directory)
     }
     console.log('header:', request.method)
-    console.log('body:', request.body)
     request
-        .on('data', (part) => {
-            console.log('part:', part)
+        .on('data', data => {
+            console.log('data:', data)
         })
         .on('end', () => {
             console.log('end')
