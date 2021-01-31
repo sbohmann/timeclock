d9102b55936d62b0eb08cb6e9978da43a7d4f3b8
sbohmann <mutabor@yeoman.at>
2021-01-31 11:06:22 +0100

    extended certificate script

--- create_certificate.sh
+++ create_certificate.sh
@@ -3,4 +3,4 @@ set -x
 openssl genrsa -out key.pem
 openssl req -new -key key.pem -out csr.pem
 openssl x509 -req -days 300 -in csr.pem -signkey key.pem -out cert.pem
-openssl x509 -outform der -in cert.pem -out csr.crt
+openssl x509 -outform der -in cert.pem -out cert.crt
