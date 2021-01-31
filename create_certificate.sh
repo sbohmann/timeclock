7a1b8f26d6cb6a5b0ce90cdd91b57e972072d8b3
sbohmann <mutabor@yeoman.at>
2021-01-31 14:35:48 +0100

    certificate script

--- create_certificate.sh
+++ create_certificate.sh
@@ -1,6 +1,8 @@
 !/usr/bin/env bash
 set -x
 openssl genrsa -out key.pem
-openssl req -new -key key.pem -out csr.pem
-openssl x509 -req -days 300 -in csr.pem -signkey key.pem -out cert.pem
-openssl x509 -outform der -in cert.pem -out cert.crt
+openssl req -new -key key.pem -out root.csr
+openssl x509 -req -days 10000 -in root.csr -signkey key.pem -out root.pem
+openssl req -new -key key.pem -out cert.csr
+openssl x509 -req -days 300 -in cert.csr -CA root.pem -CAkey key.pem -CAcreateserial -out cert.pem
+openssl x509 -outform der -in root.pem -out root.crt
