d13318d0bd7370aaf45065f562bbfcd6282ae5fc
sbohmann <mail@sebastian-bohmann.com>
2023-05-15 20:33:36 +0200

    iOS List button in scrollable vstack

--- native/ios/timeclock/timeclock/ViewController.swift
+++ native/ios/timeclock/timeclock/ViewController.swift
@@ -1,32 +1,21 @@
-//
-//  ViewController.swift
-//  timeclock
-//
-//  Created by Sebastian Bohmann on 28.04.23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var links: UIStackView!
+    @IBOutlet weak var content: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        links.alignment = .fill
-        links.distribution = .fillEqually
-        links.spacing = 0.0
-        let exampleButton = UIButton()
-        exampleButton.setTitle("Example", for: .normal)
-        exampleButton.backgroundColor = .systemBlue
-//        exampleButton.
-//        exampleButton.translatesAutoresizingMaskIntoConstraints = false
-//        exampleButton.frame.size = CGSize(width: 100, height: 100)
-        print(links.arrangedSubviews.count)
-        links.insertArrangedSubview(exampleButton, at: 1)
-        print(links.arrangedSubviews.count)
-//        links.addArrangedSubview(exampleButton)
-//        print(links.arrangedSubviews.count)
-        links.backgroundColor = .yellow
+        addButton("List", openList)
+    }
+    
+    func addButton(_ name: String, _ action: () -> Void) {
+        let button = UIButton(type: .system)
+        button.setTitle(name, for: .normal)
+        button.addAction(UIAction { _ in self.openList() }, for: .touchUpInside)
+        content.addArrangedSubview(button)
+    }
+    
+    func openList() {
+        print("Would open list")
     }
 }
